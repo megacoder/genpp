4eb6291f57959add8a2b2a540f8a9cfcdc9843ec
Tommy Reynolds <Oldest.Software.Guy@gmail.com>
2017-07-01 14:39:31 -0500

    Build targets I am interested in.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,8 @@ if [ $# -eq 0 ]; then
 	set -- bztar gztar rpm
 fi
 rm -rf build dist
-for PKG in "${@}"; do
-	python ./setup.py bdist --format="${PKG}"
-done
+(
+	for PKG in "${@}"; do
+		python ./setup.py bdist --format="${PKG}"
+	done
+) 2>&1 | tee build.log
