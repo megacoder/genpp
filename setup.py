5139e3659f92bd21227248bbb001417c04e4ac12
Tommy Reynolds <Oldest.Software.Guy@gmail.com>
2019-03-26 00:38:27 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import	glob
 import	os
 
 NAME	= 'genpp'
-VERSION = '1.1.5'
+VERSION = '1.1.6'
 
 with open( '{0}/version.py'.format( 'src' ), 'w') as f:
 	print >>f, 'Version="{0}"'.format( VERSION )
