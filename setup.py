4090536d03bd3996d030a20ae5c797dc989c0fd0
Tommy Reynolds <Oldest.Software.Guy@gmail.com>
2019-06-08 16:29:18 -0500

    v1.1.13

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import	glob
 import	os
 
 NAME	= 'genpp'
-VERSION = '1.1.12'
+VERSION = '1.1.13'
 
 with open( '{0}/version.py'.format( 'src' ), 'w') as f:
 	print >>f, 'Version="{0}"'.format( VERSION )
